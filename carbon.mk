e30c0babeffef37af8d7000b5d2e308daf138a31
Patrick Campanale <patrickappcreator@gmail.com>
2014-10-07 21:17:38 -0700

    hlte: carbonize
    ps2: remove vendorsetup & make commit message conform to standards
    Change-Id: Ib18a06c0323d13317ccf610cceffa4193ef8ce34

---
+++ carbon.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/hlteusc/full_hlteusc.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+PRODUCT_DEVICE := hlteusc
+PRODUCT_NAME := carbon_hlteusc
